9078e48bd673a410af7e1027acc9a37b318e6700
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-17 20:37:55 +0000

    scrape the supplementary news details from each section

--- run.py
+++ run.py
@@ -9,10 +9,20 @@ soup = BeautifulSoup(page.content, "html.parser")
 media_content = soup.find_all('div', class_='media__content')
 
 news_headings = []
+supporting_info = []
+links = []
 
 for each in media_content:
-    heading_text = each.find("h3").get_text()
-    heading_text = heading_text.replace('\n', '')
-    heading_text = heading_text.strip(' ')
-    news_headings.append(heading_text)
-print(news_headings)
+    # heading_text = each.find("h3").get_text()
+    # heading_text = heading_text.replace('\n', '')
+    # heading_text = heading_text.strip(' ')
+    # news_headings.append(heading_text)
+    try:
+        secondary_text = each.find("p").get_text()
+    except:
+        secondary_text = 'No supporting text supplied, access the link for more info'
+    secondary_text = secondary_text.replace('\n', '')
+    secondary_text = secondary_text.replace('"', '')
+    secondary_text = secondary_text.strip(' ')
+    supporting_info.append(secondary_text)
+print(supporting_info)
