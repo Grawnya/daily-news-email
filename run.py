ffb849604bcaa7e6ef215e46a02114f884d911f8
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-17 21:21:30 +0000

    create scraped_values() inside WebsiteInformation class to scrape html data

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import requests
+import website
 from bs4 import BeautifulSoup
 
 URL = "https://www.bbc.com/"
@@ -6,30 +7,34 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-media_content = soup.find_all('div', class_='media__content')
+media_content = website.WebsiteInformation(URL, soup)
 
-news_headings = []
-supporting_info = []
-links = []
+media_content.scraped_values('div', soup, 'media__content')
 
-for each in media_content:
-    # heading_text = each.find("h3").get_text()
-    # heading_text = heading_text.replace('\n', '')
-    # heading_text = heading_text.strip(' ')
-    # news_headings.append(heading_text)
-    link = each.find_next("a").get('href')
-    link = link.replace('\n', '')
-    link = link.strip(' ')
-    if 'https' not in link:
-        link = URL + link[1:]
-    links.append(link)
-    # try:
-    #     secondary_text = each.find("p").get_text()
-    # except:
-    #     secondary_text = 'No supporting text supplied, access the link for more info'
-    # secondary_text = secondary_text.replace('\n', '')
-    # secondary_text = secondary_text.replace('"', '')
-    # secondary_text = secondary_text.strip(' ')
-    # supporting_info.append(secondary_text)
-print(links)
+# media_content = soup.find_all('div', class_='media__content')
+
+# news_headings = []
+# supporting_info = []
+# links = []
+
+# for each in media_content:
+#     # heading_text = each.find("h3").get_text()
+#     # heading_text = heading_text.replace('\n', '')
+#     # heading_text = heading_text.strip(' ')
+#     # news_headings.append(heading_text)
+#     link = each.find_next("a").get('href')
+#     link = link.replace('\n', '')
+#     link = link.strip(' ')
+#     if 'https' not in link:
+#         link = URL + link[1:]
+#     links.append(link)
+#     # try:
+#     #     secondary_text = each.find("p").get_text()
+#     # except:
+#     #     secondary_text = 'No supporting text supplied, access the link for more info'
+#     # secondary_text = secondary_text.replace('\n', '')
+#     # secondary_text = secondary_text.replace('"', '')
+#     # secondary_text = secondary_text.strip(' ')
+#     # supporting_info.append(secondary_text)
+# print(links)
 
--- website.py
+++ website.py
@@ -1,6 +1,13 @@
 class WebsiteInformation:
 
-    def __init__(self, website_name):
+    def __init__(self, website_name, html):
         self.website_name = website_name
+        self.html = html
     
-    def scrape_values(self):
+    def scraped_values(self, tag, html, identifying_class):
+        '''docstring'''
+        website_content = html.find_all(tag, class_= identifying_class)
+        return website_content
+    
+    def headings(self):
+        '''docstring'''
