0e0a2f1e7b992fcc06424427d58680bda9d8c60d
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-17 20:32:27 +0000

    scrape the headings of the inserted website and save in list

--- run.py
+++ run.py
@@ -4,3 +4,15 @@ from bs4 import BeautifulSoup
 URL = "https://www.bbc.com/"
 page = requests.get(URL)
 
+soup = BeautifulSoup(page.content, "html.parser")
+
+media_content = soup.find_all('div', class_='media__content')
+
+news_headings = []
+
+for each in media_content:
+    heading_text = each.find("h3").get_text()
+    heading_text = heading_text.replace('\n', '')
+    heading_text = heading_text.strip(' ')
+    news_headings.append(heading_text)
+print(news_headings)
