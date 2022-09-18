ee597bf23b73cbe66beaff1c0d60722ea52be333
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-18 17:49:44 +0000

    update creds.json to include google drive API key details

--- run.py
+++ run.py
@@ -9,12 +9,3 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 media_content = website.BBC(URL, soup)
 dataframe = media_content.news_dataframe()
-
-user_email = input('put In your email below\n')
-
-contents = [
-    '<h2>Here\'s the daily news from BBC</h2>',
-    dataframe
-]
-
-yag.send(to=user_email, subject='Test123', contents='test')
